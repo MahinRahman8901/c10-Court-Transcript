73cc21bc6c71cc01b6c14a9d7e183bceee627fe0
ErvinRex <92224370+ErvinRex@users.noreply.github.com>
2024-04-22 15:12:21 +0100

    improved extract script for reusability

--- pipeline/extract.py
+++ pipeline/extract.py
@@ -1,9 +1,18 @@
+"""Python script responsible for extracting court transcript data using web scraping"""
+
+from os import environ as ENV
+
+from dotenv import load_dotenv
+
 import requests
 from bs4 import BeautifulSoup
+import logging
+from time import sleep
 
 # TODO: work on just one court for now - pass in filter variable for grabbing urls
 # TODO: use gpt to get verdict in one word
 # TODO: parse the pdf of each case rather than the html
+
 
 def get_index_to_infinity():
     """Get index to infinity"""
@@ -17,14 +26,16 @@ def scrape_law_case_urls(url: str) -> list[str]:
     """Get the url for each case on the courts webpage"""
     try:
         response = requests.get(url)
-        
+
         response.raise_for_status()
-        
+
         soup = BeautifulSoup(response.content, 'html.parser')
 
-        case_law_container = soup.find('div', class_='results__result-list-container')
-        
-        case_law_urls = case_law_container.find('ul', class_='judgment-listing__list')
+        case_law_container = soup.find(
+            'div', class_='results__result-list-container')
+
+        case_law_urls = case_law_container.find(
+            'ul', class_='judgment-listing__list')
 
         if case_law_urls:
             case_law_items = case_law_urls.find_all('a')
@@ -35,39 +46,42 @@ def scrape_law_case_urls(url: str) -> list[str]:
 
         return case_urls
     except requests.RequestException as e:
-        print(f"Error fetching URL: {e}")
+        logging.info(f"Error fetching URL: {e}")
         return []
-    
+
+
 def combine_case_url(url_list: list[str]) -> list[str]:
     """Return the combined url of case with url of webpage"""
 
     if url_list:
         case_urls = []
-        
+
         for url in url_list:
-            full_case_link = f"https://caselaw.nationalarchives.gov.uk{url}"
+            full_case_link = f"{ENV['BASE_URL']}/{url}"
             case_urls.append(full_case_link)
         return case_urls
     else:
-        print("No case law items found.")
+        logging.info("No case law items found.")
         return []
-    
-# def scrape_
+
+
+def get_case_pdf_url(case_url: str) -> str:
+    """Return the url for downloading the pdf transcript of a case"""
+    ...
 
 
 if __name__ == "__main__":
 
+    load_dotenv()
+
     for i in get_index_to_infinity():
-        query_extension = f"?query=&order=&page={i}"
-        url = "https://caselaw.nationalarchives.gov.uk/judgments/search"
+        query_extension = ENV['COMM_QUERY_EXTENSION'] + str(i)
+        url = f"{ENV['BASE_URL']}/{query_extension}"
 
         case_urls = scrape_law_case_urls(url)
 
         combined_urls = combine_case_url(case_urls)
-        if i > 5:
+        print(combined_urls)
+        sleep(1)
+        if i >= 2:
             break
-
-"""This script is responsible for extracting court transcript data"""
-
-if __name__ == "__main__":
-    pass
