967714940dcca85e7e2aea37cea8ec476063117b
luciegamby-sigmalabs <trainee.lucie.gamby@sigmalabs.co.uk>
2024-04-22 10:19:08 +0100

    Initialised pipeline folder

---
+++ pipeline/load.py
@@ -0,0 +1,4 @@
+"""This script is responsible for loading data into an RDS database"""
+
+if __name__ == "__main__":
+    pass
---
+++ pipeline/transform.py
@@ -0,0 +1,5 @@
+"""This script is responsible for transforming court transcript data into meaningful sentiments"""
+
+
+if __name__ == "__main__":
+    pass
