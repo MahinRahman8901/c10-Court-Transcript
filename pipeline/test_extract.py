e0c4b33d9c68991be89deb928196cabc3dba08cd
luciegamby-sigmalabs <trainee.lucie.gamby@sigmalabs.co.uk>
2024-04-24 17:25:22 +0100

    Added test for get_index_to_infinity

--- pipeline/test_extract.py
+++ pipeline/test_extract.py
@@ -1,7 +1,7 @@
 """This script test functions in the extract.py file"""
 from os import environ as ENV
 
-from extract import create_dataframe, combine_case_url
+from extract import create_dataframe, combine_case_url, get_index_to_infinity
 
 """
 Testing create_dataframe
@@ -38,3 +38,16 @@ def test_combine_case_url_returns_correct_url():
     urls = ["fizz"]
     case_urls = combine_case_url(urls)
     assert case_urls[0] == "real.url/fizz"
+
+
+"""
+Testing get_index_to_infinity
+"""
+
+
+def test_get_index_to_infinity():
+    indexes = get_index_to_infinity()
+    first = next(indexes)
+    second = next(indexes)
+    assert first == 1
+    assert second == 2
