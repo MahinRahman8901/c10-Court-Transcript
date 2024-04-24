a92e4af2abf96f02c9fc43c89dd1f39dde45f3f3
luciegamby-sigmalabs <trainee.lucie.gamby@sigmalabs.co.uk>
2024-04-24 14:31:30 +0100

    Added test for create_dataframe - tests that the dataframe is the correct length

--- pipeline/test_extract.py
+++ pipeline/test_extract.py
@@ -1,11 +1,18 @@
 """This script test functions in the extract.py file"""
-from extract import create_dataframe
+from extract import create_dataframe, combine_case_url
 
 
 def test_create_dataframe_drops_columns():
     test_dict_list = [{"pdf": "foo", "filepath": "bar", "title": "foobar"}]
     df = create_dataframe(test_dict_list)
     assert df.columns.values[0] == "title"
+
+
+def test_create_dataframe_returns_correct_num_items():
+    test_dict_list = [{"pdf": "foo", "filepath": "bar", "title": "foobar"}, {
+        "pdf": "fizz", "filepath": "buzz", "title": "fizzbuzz"}]
+    df = create_dataframe(test_dict_list)
+    assert len(df) == 2
 
 
 def test_passing_for_workflow():
