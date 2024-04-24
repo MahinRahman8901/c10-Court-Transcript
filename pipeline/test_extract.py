76748ef33e50169b990bc5395ea15514cb863530
luciegamby-sigmalabs <trainee.lucie.gamby@sigmalabs.co.uk>
2024-04-24 12:52:17 +0100

    Added test to make sure unimportant columns are dropped

--- pipeline/test_extract.py
+++ pipeline/test_extract.py
@@ -1,4 +1,11 @@
 """This script test functions in the extract.py file"""
+from extract import create_dataframe
+
+
+def test_create_dataframe_drops_columns():
+    test_dict_list = [{"pdf": "foo", "filepath": "bar", "title": "foobar"}]
+    df = create_dataframe(test_dict_list)
+    assert df.columns.values[0] == "title"
 
 
 def test_passing_for_workflow():
