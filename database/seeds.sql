b29c3b29b579679c5db34ecda179b9e5400f80f9
MahinRahman <trainee.mahin.rahman@sigmalabs.co.uk>
2024-04-25 15:28:19 +0100

    added seeded judge sample darta

--- database/seeds.sql
+++ database/seeds.sql
@@ -29,3 +29,11 @@ VALUES
     ('Wales'),
     ('Other Tribunal')
 ;
+
+INSERT INTO judge ("name", "appointed", "circuit_id", "judge_type_id", "gender")
+VALUES
+    ('John Doe', '2021-06-15', 1, 1, 'M'),
+    ('Jane Smith', '2019-04-23', 2, 2, 'F'),
+    ('Alice Johnson', '2020-11-01', 1, 3, 'F'),
+    ('Chris Lee', '2022-01-12', 3, 1, 'M'),
+    ('Pat Thomas', '2018-05-19', 2, 8, 'M');
