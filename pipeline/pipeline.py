9f2188ba532683b0aef3c2d54597a2c5cee7efa8
ErvinRex <92224370+ErvinRex@users.noreply.github.com>
2024-04-25 10:20:54 +0100

    dockerised and lambdarised etl

---
+++ pipeline/pipeline.py
@@ -0,0 +1,32 @@
+"""Python script that extracts, transforms and loads
+case-relevant information to a relational database service on AWS"""
+
+from extract import extract_cases
+from transform import transform_and_apply_gpt
+from load import load_to_database
+
+
+def main():
+    """Run the pipeline functions -
+    Web scrapes government case website
+    Clean, process and pass data to openai API
+    Upload case data to RDS
+    """
+
+    cases = extract_cases(1)
+
+    transform_and_apply_gpt(cases)
+
+    load_to_database(cases)
+
+
+def handler(event, context):
+    """Pass the main function to a handler to be run by Lambda on AWS"""
+
+    main()
+
+    if __name__ == "__main__":
+
+        main()
+
+        handler()
