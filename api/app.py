6476a65c97366deedf96f7e95b2cd02aefe9ee43
ayesha <ayeshaparvez0603@gmail.com>
2024-04-26 10:48:54 +0100

    skeleton files

---
+++ api/app.py
@@ -0,0 +1,9 @@
+'''Flask API for displaying data from the database.'''
+
+import psycopg2
+from dotenv import load_dotenv
+from flask import Flask
+
+
+if __name__ == "__main__":
+    pass
---
+++ api/queries.py
@@ -0,0 +1,7 @@
+'''Functions that query the database for information.'''
+
+import psycopg2
+from dotenv import load_dotenv
+
+if __name__ == '__main__':
+    pass
