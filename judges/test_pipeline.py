636477150b21ef394328e8cb860b8216c9ed3fea
luciegamby-sigmalabs <trainee.lucie.gamby@sigmalabs.co.uk>
2024-04-26 12:27:57 +0100

    Added tests for convert_date

--- judges/test_pipeline.py
+++ judges/test_pipeline.py
@@ -1,3 +1,15 @@
 """This script tests functions in the pipeline.py file"""
+import pytest
 
 from pipeline import convert_date, extract_name_gender, transform_df, concat_dfs
+
+"""
+Testing convert_date
+"""
+
+
+@pytest.mark.parametrize("input_date, expected_date", [("12-03-2024", "2024-03-12"),
+                                                       ("12-03-24", "2024-03-12"),
+                                                       ("12-Mar-24", "2024-03-12")])
+def test_convert_date_returns_correct_format(input_date, expected_date):
+    assert convert_date(input_date) == expected_date
