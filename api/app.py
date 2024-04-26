09d3c993cf0068018d571c96a4b8bf21f951996e
ayesha <ayeshaparvez0603@gmail.com>
2024-04-26 14:56:14 +0100

    setting up flask ap

--- api/app.py
+++ api/app.py
@@ -1,9 +1,20 @@
 '''Flask API for displaying data from the database.'''
 
-import psycopg2
+from psycopg2 import OperationalError
 from dotenv import load_dotenv
 from flask import Flask
+from queries import get_db_connection
+
+app = Flask(__name__)
 
 
 if __name__ == "__main__":
-    pass
+
+    load_dotenv()
+
+    app.run(debug=True, host="0.0.0.0", port=5000)
+
+    try:
+        CONN = get_db_connection()
+    except OperationalError as e:
+        print("Error while connecting to PostgreSQL", e)
