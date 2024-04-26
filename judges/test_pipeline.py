c37fc5073440ec2fa64d9dbf034227e3e56a38ed
luciegamby-sigmalabs <trainee.lucie.gamby@sigmalabs.co.uk>
2024-04-26 12:33:55 +0100

    Added docstrings

--- judges/test_pipeline.py
+++ judges/test_pipeline.py
@@ -12,9 +12,13 @@ Testing convert_date
                                                        ("12-03-24", "2024-03-12"),
                                                        ("12-Mar-24", "2024-03-12")])
 def test_convert_date_returns_correct_format(input_date, expected_date):
+    """Tests that the dates are returned in the correct format."""
+
     assert convert_date(input_date) == expected_date
 
 
 def test_convert_date_returns_datetime():
+    """Tests that a string is returned."""
+
     date = convert_date("12-03-2024")
     assert isinstance(date, str)
