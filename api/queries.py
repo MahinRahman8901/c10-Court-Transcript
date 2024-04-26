9a5a86989df0badab03ca9b2cc2a671d95935fce
ayesha <ayeshaparvez0603@gmail.com>
2024-04-26 13:17:34 +0100

    query any table

--- api/queries.py
+++ api/queries.py
@@ -1,7 +1,45 @@
 '''Functions that query the database for information.'''
 
-import psycopg2
 from dotenv import load_dotenv
+from psycopg2 import connect, sql
+from psycopg2.extras import RealDictCursor, RealDictRow
+from os import environ as ENV
+
+
+def get_db_connection(config):
+    '''Establishes connection to the database.'''
+    conn = connect(user=config["DB_USER"],
+                   password=config["DB_PASSWORD"],
+                   host=config["DB_HOST"],
+                   port=config["DB_PORT"],
+                   database=config["DB_NAME"],
+                   cursor_factory=RealDictCursor)
+    return conn
+
+
+def get_table(conn, table: str) -> list[RealDictRow]:
+    '''Returns table information as a list. 
+    Only accepts table names: circuit, court_case, judge and judge_type.'''
+
+    if table in ['circuit', 'court_case', 'judge', 'judge_type']:
+
+        with conn.cursor() as cur:
+
+            cur.execute(
+                sql.SQL("SELECT * FROM {};").format(sql.Identifier(table)))
+            rows = cur.fetchall()
+
+        return rows
+
+    return {'ERROR': f'Table {table} does not exist.'}
+
 
 if __name__ == '__main__':
-    pass
+
+    load_dotenv()
+
+    CONN = get_db_connection(ENV)
+
+    result = get_table(CONN, 'court_case')
+
+    print(type(result[0]))
