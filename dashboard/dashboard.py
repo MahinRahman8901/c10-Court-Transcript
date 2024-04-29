3c8f2fe1d350d7e718bda669eec03ba595cf1975
RachelPun <rachelpunhokyan@ymail.com>
2024-04-29 15:26:00 +0100

    done dashboard judge profile script

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from os import environ as ENV
+import re
 from datetime import datetime, timezone, timedelta
 from dateutil.relativedelta import relativedelta
 from dotenv import load_dotenv
@@ -22,19 +23,100 @@ def get_db_connection(config) -> connect:
                    cursor_factory=RealDictCursor)
 
 
+def extract_id_from_string(string: str) -> int:
+    """Returns id in a bracket before string info."""
+    return int(re.match(r"\((\d+)\)", string).group(1))
+
+
+def get_judge_selection(conn: connect) -> st.selectbox:
+    """Returns a Streamlit selectbox for individual judges."""
+
+    with conn.cursor() as cur:
+        query = """
+                SELECT FORMAT('(%s) %s', judge_id, name) judge
+                FROM judge
+                """
+        cur.execute(query)
+        rows = cur.fetchall()
+
+    rows = [item["judge"] for item in rows]
+
+    judge_selection = st.selectbox(placeholder="select a judge",
+                                   options=rows,
+                                   index=None,
+                                   label="judge selection",
+                                   label_visibility="hidden")
+
+    return judge_selection
+
+
+def get_judge_from_db(conn: connect, id: int) -> tuple[dict, list[dict]]:
+    """Returns a tuple of judge info and cases overseen."""
+
+    with conn.cursor() as cur:
+        judge_query = """
+                        WITH judge_selected AS (
+                            SELECT *
+                            FROM judge
+                            WHERE judge_id = %s
+                        )
+                        SELECT j.name, j.gender, j.appointed, t.type_name AS type, c.name AS circuit
+                        FROM judge_selected AS j
+                        JOIN judge_type AS t
+                            ON j.judge_type_id = t.judge_type_id
+                        JOIN circuit AS c
+                            ON j.circuit_id = c.circuit_id
+                        """
+        cur.execute(judge_query, [id])
+        judge = cur.fetchone()
+
+        case_query = """
+                        SELECT transcript_id AS id, case_no AS "case no", title
+                        FROM transcript
+                        WHERE judge_id = %s
+                        """
+        cur.execute(case_query, [id])
+        cases = cur.fetchall()
+
+    return judge, cases
+
+
+def write_judge_profile(judge: dict) -> str:
+    """Returns a formatted string of judge info."""
+
+    return f"""name: {judge["name"]}  |  gender: {judge["gender"]}\n
+judge type: {judge["type"]}\n
+circuit: {judge["circuit"]}\n
+appointed: {judge["appointed"]}
+"""
+
+
 if __name__ == "__main__":
 
     load_dotenv()
     conn = get_db_connection(ENV)
 
+    st.set_page_config(page_title="Arelm Court Dashboard", page_icon="🏛", layout="wide",
+                       initial_sidebar_state="expanded", menu_items=None)
+
     with st.sidebar:
         st.title("Arelm")
-        st.subheader("")
+        st.subheader("lorem ipsum something about Arelm and the dashboard")
+        st.text("")
 
-    profiles, visualizations = st.columns([.4, .6], gap="medium")
+    profiles, visualizations = st.columns([.35, .65], gap="medium")
     with profiles:
         # judge profile
-        pass
+        judge_profile_selection = get_judge_selection(conn)
+        if judge_profile_selection:
+            id = extract_id_from_string(judge_profile_selection)
+            judge, cases = get_judge_from_db(conn, id)
+            profile = write_judge_profile(judge)
+            st.write(profile)
+            st.dataframe(cases, hide_index=True,
+                         use_container_width=True)
+        else:
+            st.write("*(no judge selected)*")
 
         # court case profile
         pass
